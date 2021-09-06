02ad54df13789fc4e0b3513bb6572776b9f7aa6c
mohyeddineamine@gmail.com <aminemohyeddine@gmail.com>
2021-09-06 06:39:24 +0100

    m

--- server.js
+++ server.js
@@ -37,14 +37,6 @@ app.use("/dev", devRoute);
 app.use("/gamedata", gameHandlerRoute);
 app.use("/", adminAuthRoute);
 
-//serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder to serve
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 //ssl config
 portHTTPS = process.env.PORT || 3005;
 const sslServer = https.createServer(
@@ -54,6 +46,14 @@ const sslServer = https.createServer(
   },
   app
 );
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder to serve
+  sslServer.use(express.static("client/build"));
+  sslServer.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //
 sslServer.listen(portHTTPS, () => {
   console.log("server started at port " + portHTTPS);
